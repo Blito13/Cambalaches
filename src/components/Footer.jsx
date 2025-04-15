6fae4d9303c74b902c21b6b55ef3eeaa1fa1a8d9
blito13 <blito13@github.com>
2025-04-15 12:41:01 -0300

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+
+export const Footer = () => {
+    return(
+        <div className="footer-container">
+            <div className="footer">
+                <h1 className="footer-logo">Cambalache®</h1>
+                <ul className="lista-contacto">
+                    <li class="fa fa-whatsapp fa-lg" ></li>
+                    <li class="fa fa-facebook fa-lg" ></li>
+                    <li class="fa fa-apple fa-lg" ></li>
+                    <li class="fa fa-twitter fa-lg" ></li>
+                </ul>
+                <a href="" className="text-contact">2025 Camabalache® by Blito</a>
+            </div>
+        </div>
+    )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,15 @@ import { ProductList } from "./ProductList";
 import "./Home.css";
 import { NavBar } from "./NavBar";
 import { Hero } from "./Hero";
+import { Footer } from "./Footer";
+
 export const Home = () => {
 return(
     <div className="home-container">
     <NavBar></NavBar>
     <Hero></Hero>
     <ProductList/>
+    <Footer></Footer>
     </div>
 )
 };
