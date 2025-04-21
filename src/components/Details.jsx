aa96d03c8ff1d26a149186bc05867b4301200a9b
blito13 <blito13@github.com>
2025-04-21 18:45:58 -0300

    styles responsive

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -15,7 +15,9 @@ export const Details = () =>{
     const data = useSelector((state)=> state.products);
     const element = data.products.filter((p) => p.id === id);
     if (!element) return <div>Producto no encontrado</div>;
+    const [selected, setSelected] = useState(element[0].thumbnail[0]);
     const text = `Hola necesito informacion acerca de ${ element[0].title}`;
+    console.log(selected, "morreja")
     const contact = (e ) => {
         e.preventDefault();
         let form = {
@@ -25,23 +27,38 @@ export const Details = () =>{
     
         dispatch(contactOwner(form))
       };
+
     return(
-        <div className="product">
-            <Link to={"/"}>Go back Jo-Jo</Link>
-            <h1>
+        <div className="image-carousel">
+            {/* <h1>
             {element[0].title}
             </h1>
             <h1>
             {element[0].description}
-            </h1>  
-            <div className="img-container">
-                {
-                    element[0].thumbnail.map((e) =>
-                    <img src={e} alt="pic" />
+            </h1>   */}
+            <div className="main-image-container">
+                <img 
+                src={selected}
+                alt="pic" 
+                className="main-image"
+                />
+            </div>
+            <div className="thumbnail-container">
+                {element[0].thumbnail.map((e, i) => (
+                    <div
+                    className={selected=== e ?"thumbnail-selected" : "thumbnail"}
+                    onClick={()=>setSelected(element[0].thumbnail[i]) }
+                    >
+                        <img key={i} src={e}  alt={`Miniatura ${i + 1}`}  />
+                    </div>
                     )
-                }
+
+                    
+                )
+            }
             </div>
         <>
+            <Link to={"/"}>Go back Jo-Jo</Link>
           <IconContext.Provider value={{ color: "blue", size : "1em"}}>
                   <button onClick={(e)=>contact(e)}>
                     <SiWhatsapp/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,21 @@
 import "./NavBar.css";
+import { useState } from "react";
 
 
 
 export const NavBar = () =>{
+const [active , setActive] = useState(false);
+const handleClick = () => {
+    setActive(!active);
+    console.log(active);
+};
     return(
         <div className="navbar">
             <div className="nav-container">
                 <h2 className="logo">Cambalache®</h2>
+                <button className="btn" onClick={handleClick}>
+                    {active === false ? "O" : "X"}
+                </button>
                 <nav>
                     <ul>
                         <li className="nav"><a href="" className="outline">Descargas</a></li>
